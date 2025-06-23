ac165c3ba8e87710150af835c936a1389e6947be
Vipul Meshram <vipulmeshramv20@gmail.com>
2025-06-23 12:28:57 +0530

    Add Source File

---
+++ HotelManagement.java
@@ -0,0 +1,6 @@
+// Replace this with your full HotelManagement.java source code
+public class HotelManagement {
+    public static void main(String[] args) {
+        System.out.println("Hotel Management System");
+    }
+}
